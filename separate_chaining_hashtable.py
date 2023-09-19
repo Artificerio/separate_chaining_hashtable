7c26b7cb7e828e4d865432cf168fb54d82a6305b
1111 <a1111@MacBook-Air-1111.local>
2023-09-19 22:37:19 +0300

    type hints and other methods

--- separate_chaining_hashtable.py
+++ separate_chaining_hashtable.py
@@ -1,5 +1,9 @@
+import typing
+from typing import Hashable, Any
+
+
 class Node:
-    def __init__(self, key, value):
+    def __init__(self, key: typing.Hashable, value: Any) -> None:
         """linked list cell in form of:
         key -> value"""
         self.key = key
@@ -10,17 +14,20 @@ class Node:
 class HashTable:
     """It is important ot choose the right hash table size
     to maximize performance and reduce collisions"""
-    def __init__(self, capacity):
+    def __init__(self, capacity: int) -> None:
         self.capacity = capacity
         self.size = 0
         self.table = [None] * capacity
 
-    def _hash(self, key) -> int:
+    def _hash(self, key: typing.Hashable) -> int | str:
         """generate hash and take it modulo size,
         it fits in the table"""
-        return hash(key) % self.capacity
+        try:
+            return hash(key) % self.capacity
+        except TypeError:
+            return "unhashable type"
 
-    def insert(self, key, value):
+    def insert(self, key: typing.Hashable, value: Any) -> None:
         """insert value in the table
         if linked list cell is None, initialize it and assign the value
         else if collision occurred, go to the end of the list and insert value"""
@@ -41,7 +48,7 @@ class HashTable:
             self.table[index] = new_node
             self.size += 1
 
-    def search(self, key):
+    def search(self, key: typing.Hashable) -> Any:
         """Search for the value based on key
         if item is not found, KeyError is raised"""
         index = self._hash(key)
@@ -54,7 +61,7 @@ class HashTable:
 
         raise KeyError(key)
 
-    def remove(self, key):
+    def remove(self, key: typing.Hashable) -> None:
         """Delete value from the table"""
         index = self._hash(key)
 
@@ -74,29 +81,43 @@ class HashTable:
 
         raise KeyError(key)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.size
 
-    def __contains__(self, key):
+    def __contains__(self, key) -> bool:
         try:
             self.search(key)
             return True
         except KeyError:
             return False
 
-    def __getitem__(self, key):
+    def __getitem__(self, key) -> int | str:
         try:
             value = self.search(key)
             return value
         except KeyError:
             return "value does not exist"
 
+    def __str__(self) -> str:
+        """Return hash table elements"""
+        elements = []
+        for i in range(self.capacity):
+            current = self.table[i]
+            while current:
+                elements.append((current.key, current.value))
+                current = current.next
+        return str(elements)
 
 
 if __name__ == '__main__':
-
     ht = HashTable(5)
     ht.insert("apple", 3)
     ht.insert("banana", 2)
     ht.insert("cherry", 5)
+    ht.insert(12, [1, 2, 3])
+    print(ht)
     print(ht['zxc'], 'zxc' in ht)
+    ht.insert('apple', 15)  # update value at apple
+    print(ht)
+    ht.remove('banana')
+    print(ht)
